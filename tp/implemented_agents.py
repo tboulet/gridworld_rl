0020af2c81106e944a5ce0f3e2ec030759d5caff
tboulet <timothe.boulet0@gmail.com>
2024-12-02 23:11:02 +0100

    build tp version

--- tp/implemented_agents.py
+++ tp/implemented_agents.py
@@ -1,12 +1,8 @@
 from tp.agents.random import RandomAgent
-from tp.agents.sarsa import SarsaAgent
 from tp.agents.qlearning import QLearningAgent
-from tp.agents.montecarlo import MonteCarloAgent
 
 agents_map = {
     "random": RandomAgent,
-    "sarsa" : SarsaAgent,
     "qlearning" : QLearningAgent,
-    "mc" : MonteCarloAgent
 
 }
